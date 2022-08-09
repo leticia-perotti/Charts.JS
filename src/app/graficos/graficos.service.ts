336dd59b566efe3aa4583a55175683d4d10cf40d
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-08-08 21:41:32 -0300

    Grafico de linhas

--- src/app/graficos/graficos.service.ts
+++ src/app/graficos/graficos.service.ts
@@ -12,4 +12,8 @@ export class GraficosService {
   barras(){
     return this.http.get(this.local+"acessoaonivelsuperior")
   }
+
+  precipitacao(){
+    return this.http.get(this.local+"precipitacao")
+  }
 }
--- src/app/graficos/linhas/linhas.component.ts
+++ src/app/graficos/linhas/linhas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { GraficosService } from '../graficos.service';
 
 @Component({
   selector: 'app-linhas',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./linhas.component.scss']
 })
 export class LinhasComponent implements OnInit {
+  rows : any[] = []
+  labels: any[] = []
+  dado1: any[] = []
+  dado2: any[] = []
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: GraficosService) {
+    Chart.register(...registerables);
   }
 
+  ngOnInit(): void {
+    this.listar()
+  }
+
+  listar(){
+    this.service.precipitacao().subscribe((data: any) => {
+      this.rows = data
+
+      this.extractLabel()
+      this.extractDataset()
+
+      var myChart = new Chart("myChart", {
+        type: 'line',
+        data: {
+          labels: this.labels,
+          datasets:[{
+            label: "2016",
+            data : this.dado1,
+            fill: true,
+            backgroundColor: "#F4BFBF8a"
+          },
+          {
+            label: "2016",
+            data : this.dado2,
+            fill: true,
+            backgroundColor: "#8CC0DE8a"
+          },
+        ]
+        }
+      });
+    })
+  }
+
+  extractLabel(){
+    this.rows.map((row:any) => {
+      this.labels.push(row.mes)
+    })
+  }
+  extractDataset(){
+    this.rows.map((row:any) => {
+      this.dado1.push(row.dado1)
+      this.dado2.push(row.dado2)
+
+    })
+  }
 }
+
