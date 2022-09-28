92116f4d2de7002b2778622a6a647c0f4f91a242
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-09-27 21:59:08 -0300

    Filtro graficos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'charts';
 
-  pesquisa: any
+  pesquisa: string = ''
+
+  seleciona(){
+    var filter = this.pesquisa.toUpperCase();
+    var lis = document.getElementsByTagName('li')!!;
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].getElementsByClassName('name')[0].innerHTML!!;
+        if (name.toUpperCase().indexOf(filter) == 0)
+            lis[i].style.display = 'list-item';
+        else
+            lis[i].style.display = 'none';
+    }
+  }
 }
